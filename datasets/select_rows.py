068800cadfab17b19f75b1455ed5d0ef2bd04192
Lan Li <lilan.scut@gmail.com>
2024-10-13 21:55:00 -0500

    script selecting rows

--- datasets/select_rows.py
+++ datasets/select_rows.py
@@ -9,8 +9,9 @@ df = pd.read_csv('original/Dish.csv')
 # Shuffle the rows randomly
 df_shuffled = df.sample(frac=1, random_state=42)
 
-# Select the first 200 rows
+# Select the first n rows
 # df_selected = df_shuffled.head(1000)
+# df_selected = df_shuffled.head(100)
 df_selected = df_shuffled.head(500)
 
 # Optionally, you can save the selected rows to a new CSV file
